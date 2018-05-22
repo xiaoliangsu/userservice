80bc770b3183dc81e6d4931aefee466d798abf97
zhangyunlong <496933982@qq.com>
2018-05-22 21:23:51 +0800

    完成

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,16 +1,12 @@
 package com.example.demo;
 
 import config.GlobalCorsConfig;
-import controller.AddDeviceController;
-import controller.DeviceDetailController;
-import controller.MeasureController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import controller.UserController;
